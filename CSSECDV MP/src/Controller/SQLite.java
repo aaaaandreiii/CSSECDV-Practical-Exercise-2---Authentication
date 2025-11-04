e7cd06cce1c554bedd39de93c32a07c38f1fa117
devonjavier <devonjarekjavier@gmail.com>
2025-11-05 06:03:25 +0800

    Added prepared statement to addUsers with string role

--- CSSECDV MP/src/Controller/SQLite.java
+++ CSSECDV MP/src/Controller/SQLite.java
@@ -193,23 +193,29 @@ public class SQLite {
         }
     }
     
-    public void addUser(String username, String password) {
-        String sql = "INSERT INTO users(username, password, role) VALUES(?, ?, ?)";
-        
-        try (Connection conn = DriverManager.getConnection(driverURL);
-            Statement stmt = conn.createStatement()){
-            stmt.execute(sql);
-            
 //      PREPARED STATEMENT EXAMPLE
 //      String sql = "INSERT INTO users(username,password) VALUES(?,?)";
 //      PreparedStatement pstmt = conn.prepareStatement(sql)) {
 //      pstmt.setString(1, username);
 //      pstmt.setString(2, password);
 //      pstmt.executeUpdate();
+    public void addUser(String username, String password, String role) {
+        String sql = "INSERT INTO users(username, password, role) VALUES(?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(driverURL);
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.setString(3, role);
+
+            pstmt.executeUpdate();
+
         } catch (Exception ex) {
-            System.out.print(ex);
+            System.out.println("Error inserting user: " + ex.getMessage());
         }
     }
+
     
     
     public ArrayList<History> getHistory(){
@@ -292,7 +298,7 @@ public class SQLite {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        
+
         return users;
     }
     
